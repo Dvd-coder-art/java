a802574572c89e7c49f51d7d68452ac73a3d1a53
Dvd-coder-art <daviddavi1221@gmail.com>
2025-02-03 08:09:19 -0300

    Alterações

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,13 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        short numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 1;
+
+        numero = 2;
     }
 }
